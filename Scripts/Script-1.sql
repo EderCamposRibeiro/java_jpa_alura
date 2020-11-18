057ce1e7b184e64797c7cf6cd0a377d3e149f1e0
Eder Campos Ribeiro <edercribeiro@gmail.com>
2020-11-17 21:46:34 -0300

    03.Funções de Agregação e Group By

--- Scripts/Script-1.sql
+++ Scripts/Script-1.sql
@@ -1,3 +1,12 @@
 select * from conta c 
 
 select * from movimentacao m2  
+
+select sum(valor) from movimentacao m 
+
+select avg(valor) from movimentacao m 
+
+insert into Movimentacao (data, valor, conta_id, descricao, tipoMovimentacao) values ('2017-01-12 18:01:07', 80.0, 2, 'Restaurante', 1);
+insert into Movimentacao (data, valor, conta_id, descricao, tipoMovimentacao) values ('2017-01-12 19:31:12', 100.0, 2, 'Cinema', 1);
+insert into Movimentacao (data, valor, conta_id, descricao, tipoMovimentacao) values ('2017-01-13 10:01:54', 40.0, 2, 'Café da manhã', 1);
+insert into Movimentacao (data, valor, conta_id, descricao, tipoMovimentacao) values ('2017-01-14 15:20:13', 20.0, 2, 'Lanche', 1);
---
+++ src/main/java/br/com/alura/jpa/modelo/MediaComData.java
@@ -0,0 +1,36 @@
+package br.com.alura.jpa.modelo;
+
+public class MediaComData {
+
+	private Double valor;
+
+	private Integer dia;
+
+	private Integer mes;
+
+	private Integer ano;
+
+	public Double getValor() {
+		return valor;
+	}
+
+	public MediaComData(Double valor, Integer dia, Integer mes, Integer ano) {
+		this.valor = valor;
+		this.dia = dia;
+		this.mes = mes;
+		this.ano = ano;
+	}
+
+	public Integer getDia() {
+		return dia;
+	}
+
+	public Integer getMes() {
+		return mes;
+	}
+
+	public Integer getAno() {
+		return ano;
+	}
+
+}
